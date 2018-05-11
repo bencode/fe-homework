eff2bcd10e200fce59c7ede4fd8622c5a10534de
lola <18068001070@163.com>
2018-05-11 12:09:55 +0800

    增加删除功能

--- src/15/js/reading.js
+++ src/15/js/reading.js
@@ -6,8 +6,12 @@ const newWords=reading.querySelector('.new-words');
 const oldArticleContent=articleInputBox.querySelector('.old-article-content');
 const newArticleContent=articleShowPanel.querySelector('.new-article-content');
 const newWordsList=newWords.querySelector('.new-words-list');
-let newWordItem=null;
+const deleteBtn=reading.querySelector('.delete-btn');
+const upBtn=reading.querySelector('.up-btn');
+const downBtn=reading.querySelector('.down-btn');
+let newWordsItem=null;
 let wordList=null;
+let selectedWord=null;
 
 articleInputBox.addEventListener('blur',function(){
     const oldArticleValue=oldArticleContent.value.trim();
@@ -26,22 +30,47 @@ function addNewWord(){
         item.addEventListener('dblclick',function(){
             const newWord=item.innerHTML;
             newWordsList.innerHTML+='<li class="list-group-item">'+newWord+'</li>';
-            newWordItem=newWordsList.querySelectorAll('li');
+            newWordsItem=newWordsList.querySelectorAll('li');
             selectedWordList();
         });
     });
 }
 function selectedWordList(){
-    newWordItem.forEach(function(item){
+    newWordsItem.forEach(function(item){
         item.addEventListener('click',function(){
-            newWordItem.forEach(function(item){
+            newWordsItem.forEach(function(item){
                 item.classList.remove('active');
             });
             item.classList.add('active');
+            selectedWord=item;
         });
     });
 }
-function deleteWord(){
 
+//删除单词
+deleteBtn.addEventListener('click',function(){
+    onDeleteWord(selectedWord);
+});
+function onDeleteWord(node){
+    newWordsList.removeChild(node);
+}
+
+//上移单词
+upBtn.addEventListener('click',function(){
+    onUpSelecteWord(selectedWord);
+});
+function onUpSelecteWord(node){
+    const nextNode=node.nextSibling;
+    // console.log(previousNode);
+    newWordsList.insertBefore(node,nextNode);
+}
+
+//下移单词
+upBtn.addEventListener('click',function(){
+    onDownSelecteWord(selectedWord);
+});
+function onDownSelecteWord(node){
+    // const nextNode=node.nextSibling;
+    // newWordsList.insertBefore(nextNode,node);
 }
 

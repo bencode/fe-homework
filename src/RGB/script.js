efcbaa92b56112705bc3a5bac674ba72f57967b6
bencode <bencode@163.com>
2018-05-15 13:07:52 +0800

    finish review

--- src/RGB/script.js
+++ src/RGB/script.js
@@ -1,12 +1,12 @@
 function doYourWork() {
-var lable = document.querySelector('.color-mixer .label');
-lable.innerHTML = 'hello word';//*单引号中的内容是可以更改的*
+  var lable = document.querySelector('.color-mixer .label');
+  lable.innerHTML = 'hello word';//*单引号中的内容是可以更改的*
 
-var color = 'rgb('+ red + ',' + green + ',' +blue + ')';
-lable.innerHTML = color;
+  var color = 'rgb('+ red + ',' + green + ',' +blue + ')';
+  lable.innerHTML = color;
 
-var indicator = document.querySelector('.color-mixer .indicator');
-console.log(indicator);
-indicator.style.backgroundColor = color;//background-color
-lable.style.fontSize = '20px';//修改元素的样式
+  var indicator = document.querySelector('.color-mixer .indicator');
+  console.log(indicator);
+  indicator.style.backgroundColor = color;//background-color
+  lable.style.fontSize = '20px';//修改元素的样式
 }

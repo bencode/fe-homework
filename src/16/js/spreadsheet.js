f175efd1eee384b6ac18fe2801e90ff99c03fa55
lola <18068001070@163.com>
2018-06-20 09:44:13 +0800

    电子表格增加行

--- src/16/js/spreadsheet.js
+++ src/16/js/spreadsheet.js
@@ -1,14 +1,8 @@
 const table=document.querySelector('.table');
 const tBody=table.querySelector('tbody');
-const td=tBody.querySelector('td');
-
-function app(parentNode,eventType,node,fn){
-    parentNode.addEventListener(eventType,(event)=>{
-        if(event.target.matches(node)){
-            fn.apply(event.target,arguments);
-        }
-    });
-}
+const btns=document.querySelector('.btn');
+const addtr=btns.querySelector('.addtrbtn');
+const addth=btns.querySelector('.addthbtn');
 
 app(tBody,'click','.cell',function(){
     this.classList.add('focus');
@@ -20,8 +14,36 @@ app(tBody,'click','.cell',function(){
 app(tBody,'focusout','.input-cell',function(){
     this.parentNode.parentNode.classList.remove('focus');
     const ancestor=this.parentNode.parentNode;
-    ancestor.innerHTML='';
+    if(!(/\s/.test(this.value))){
+        ancestor.innerHTML=this.value;
+    }
+    else{
+        ancestor.innerHTML='';
+    }
+    
 });
 
+function app(parentNode,eventType,node,fn){
+    parentNode.addEventListener(eventType,(event)=>{
+        if(event.target.matches(node)){
+            fn.apply(event.target,arguments);
+        }
+    });
+}
 
+//增加行
+//tbody包括几个tr
+//tr包括几个td,包括几个class=cell的td
+let trLists=tBody.querySelectorAll('tr');
+let tdLists=trLists[0].querySelectorAll('td');//6
+let len=trLists.length;
 
+let i=0;
+
+addtr.addEventListener('click',function(){
+    const trNode=document.createElement('tr');
+    const tdNode=document.createElement('td');
+    console.log(trNode);
+
+    tBody.appendChild(trNode);
+});

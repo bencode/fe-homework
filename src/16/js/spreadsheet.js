9a09126eaaf4b5fa7a95d9beff52c512954246f5
lola <18068001070@163.com>
2018-06-25 16:29:03 +0800

    电子表格的全部功能、url解析、query解析

--- src/16/js/spreadsheet.js
+++ src/16/js/spreadsheet.js
@@ -14,13 +14,28 @@ function app(parentNode,eventType,node,fn){
         if(event.target.matches(node)){
             fn.apply(event.target,arguments);
         }
+        else{
+            let targetParent=event.target.parentNode;
+            while(!targetParent.matches(node)){
+                targetParent=targetParent.parentNode;
+            }
+            fn.apply(targetParent,arguments);
+        }
     });
 }
 
 function closest(elm,selector){
-    const element=document.querySelector(elm);
-    selector
-    return 
+    if(elm.matches(selector)){
+        return elm;
+    }
+    else{
+        let selectorElm=elm;
+        while(!selectorElm.matches(selector)){
+            selectorElm=selectorElm.parentNode;
+        }
+        return selectorElm;
+        
+    }
 }
 
 app(tBody,'click','.cell',function(){
@@ -31,11 +46,12 @@ app(tBody,'click','.cell',function(){
 });
 
 app(tBody,'focusout','.input-cell',function(){
-    this.parentNode.parentNode.classList.remove('focus');
+    // this.parentNode.parentNode.classList.remove('focus');
+    closest(this,'.cell').classList.remove('focus');
     // 使用在jquery中，一般不会使用parentNode.parentNode这种和节点层次相关的方法，因为容易出bug
     // 而是会使用 $(this).closest('.cell') 来直接找到指定选择器的父节点
     // 尝试实现closest函数，函数签名为: closest(elm, selector) -> {Element}
-    const ancestor=this.parentNode.parentNode;
+    const ancestor=closest(this,'.cell');
     if(!(/\s/.test(this.value))){
         //判断是否是数字
         if((/^\d+\.?\d*$/).test(this.value)){
@@ -54,70 +70,55 @@ app(tBody,'focusout','.input-cell',function(){
     let trLists=tBody.querySelectorAll('tr');
     const regSum=/^=sum\(([A-z]\d):([A-z]\d)\)$/;
     const regMean=/^=mean\(([A-z]\d):([A-z]\d)\)$/;
+
     if(regSum.test(this.value)){
-
-        const firstSum=RegExp.$1;
-        const lastSum=RegExp.$2;
-        const listSum1=firstSum.split('');
-        const listSum2=lastSum.split('');
-        const firstcolSum=(listSum1[0].charCodeAt(0))%65+1;
-        const lastcolSum=(listSum2[0].charCodeAt(0))%65+2;
-        const firstrowSum=parseInt(listSum1[1])-1;
-        const lastrowSum=parseInt(listSum2[1]);
-
-        let sum1=0;
-
-        //2层循环表格
-        for(var i=firstrowSum;i<lastrowSum;i++){
-            for(var j=firstcolSum;j<lastcolSum;j++){
-                let tdListsSum=trLists[i].querySelectorAll('td'); 
-                if(parseFloat(tdListsSum[j].innerHTML)){
-                    sum1+=parseFloat(tdListsSum[j].innerHTML);
-                }
-                ancestor.innerHTML=sum1;
-                ancestor.classList.add('num');
-            }
-        }
+        const numData=getSum(trLists);
+        ancestor.innerHTML=numData.sum;
+        ancestor.classList.add('num');
 
     }
     //求平均值
     if(regMean.test(this.value)){
-
-        const first=RegExp.$1;
-        const last=RegExp.$2;
-        const list1=first.split('');
-        const list2=last.split('');
-        const firstcol=(list1[0].charCodeAt(0))%65+1;
-        const lastcol=(list2[0].charCodeAt(0))%65+2;
-        const firstrow=parseInt(list1[1])-1;
-        const lastrow=parseInt(list2[1]);
-
-        let sum=0;
-
-        //2层循环表格
-        for(var m=firstrow;m<lastrow;m++){
-            for(var n=firstcol;n<lastcol;n++){
-                let tdLists=trLists[m].querySelectorAll('td'); 
-                if(parseFloat(tdLists[j].innerHTML)){
-                    sum+=parseFloat(tdLists[j].innerHTML);
-                }
-                ancestor.innerHTML=sum/((lastcol-firstcol)*(lastrow-firstrow));
-                ancestor.classList.add('num');
-            }
-        }
-
+        const numData=getSum(trLists);
+        ancestor.innerHTML=numData.sum/numData.divisor;
+        ancestor.classList.add('num');
     }
 });
+
+function getSum(trArr){
+
+    const list1=RegExp.$1.split('');
+    const list2=RegExp.$2.split('');
+    const firstcol=(list1[0].charCodeAt(0))%65+1;
+    const lastcol=(list2[0].charCodeAt(0))%65+2;
+    const firstrow=parseInt(list1[1])-1;
+    const lastrow=parseInt(list2[1]);
+    const divisor=(lastcol-firstcol)*(lastrow-firstrow);
+    let sum=0;
+
+    //2层循环表格
+    for(var i=firstrow;i<lastrow;i++){
+        for(var j=firstcol;j<lastcol;j++){
+            let tdLists=trArr[i].querySelectorAll('td'); 
+            if(parseFloat(tdLists[j].innerHTML)){
+                sum+=parseFloat(tdLists[j].innerHTML);
+            }
+        }
+    }
+    return {
+        'sum':sum,
+        'divisor':divisor
+    };
+}
 
 //增加行
 //tbody包括几个tr
 //tr包括几个td,包括几个class=cell的td
 addtr.addEventListener('click',function(){
-
     const trNode=document.createElement('tr');
-    let trLists=tBody.querySelectorAll('tr');
-    let tdLists=trLists[0].querySelectorAll('td');//6
-    let len=trLists.length;
+    const trLists=tBody.querySelectorAll('tr');
+    const tdLists=trLists[0].querySelectorAll('td');//6
+    const len=trLists.length;
 
     for(var i=0;i<tdLists.length;i++){
         const tdNode=document.createElement('td');
@@ -138,16 +139,18 @@ addtr.addEventListener('click',function(){
 //在每个tr的结尾处加上个td
 //怎样使英文字母往后递增（利用ASCII）fromCharCode
 addth.addEventListener('click',function(){
-    let trLists=tBody.querySelectorAll('tr');
-    const tdNode1=document.createElement('th');
-    let tHeadTh=tHeadTr.querySelectorAll('th');
-    let len=tHeadTh.length;
-    tdNode1.innerHTML=String.fromCharCode(65+len-1);
-    tHeadTr.appendChild(tdNode1);
+    const trLists=tBody.querySelectorAll('tr');
+    const thNode=document.createElement('th');
+    const tHeadTh=tHeadTr.querySelectorAll('th');
+    const len=tHeadTh.length;
+
+    thNode.innerHTML=String.fromCharCode(65+len-1);
+    tHeadTr.appendChild(thNode);
+
     for(var i=0;i<trLists.length;i++){
-        const tdNode2=document.createElement('td');
-        tdNode2.classList.add('cell');
-        trLists[i].appendChild(tdNode2);
+        const tdNode=document.createElement('td');
+        tdNode.classList.add('cell');
+        trLists[i].appendChild(tdNode);
     }
 });
 
